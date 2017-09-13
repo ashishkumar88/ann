27ce81550d735eca19dc048fd917e4b5d8333674
Yash Rathore <rathore.yash121@gmail.com>
2017-09-13 15:31:17 -0700

    csv file loading done

---
+++ Util.h
@@ -0,0 +1,75 @@
+#ifndef UTIL
+#define UTIL
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std ;
+
+class Util {
+    public:
+        vector<string> split(const string &text, char sep) {
+            std::vector<std::string> tokens;
+            std::size_t start = 0, end = 0;
+            while ((end = text.find(sep, start)) != std::string::npos) {
+              tokens.push_back(text.substr(start, end - start));
+              start = end + 1;
+            }
+            tokens.push_back(text.substr(start));
+            return tokens;
+        }
+
+        template <size_t rows, size_t cols>
+        void loadCSV(string fileName, float (&array)[rows][cols]){
+            ifstream myfile ( fileName ); 
+            string value;
+            std::string::size_type sz;     // alias of size_t
+
+            if (myfile.is_open())
+            {
+                int rowCount = 0;
+                int colCount = 0;
+                vector<string> tokens ;
+                std::string::size_type sz;     // alias of size_t
+
+                while ( myfile.good() )
+                {        
+                    getline ( myfile, value );      
+
+                    if(value=="")
+                        break;
+
+                    tokens = split(value, ',');
+                    for(int i=0;i<tokens.size();i++){
+                        array[rowCount][colCount]= std::stof (tokens[i],&sz);
+                        colCount++;
+                    }
+
+                    if(colCount == cols){
+                        rowCount++ ;
+                        colCount= 0 ;
+                        tokens.clear();
+                    }                    
+                }
+
+            myfile.close();
+            }
+            else cout << "Unable to open file"<<endl;
+        }
+
+        template <size_t rows, size_t cols>
+        void printData(float (&array)[rows][cols]){
+            for(int i=0; i<rows; i++)    //This loops on the rows.
+            {
+                for(int j=0; j<cols; j++) //This loops on the columns
+                {
+                    cout << array[i][j]  << "  ";
+                }
+                cout << endl;
+            }
+        }
+
+};
+
+#endif
--- ann.cpp
+++ ann.cpp
@@ -1,16 +1,29 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
+
 #include "Network.h"
 #include "NetworkLayer.h"
+#include "Util.h"
+
 
 using namespace std;
 
 int main() {
     Network ann;
-    NetworkLayer inputLayer(2);
+    NetworkLayer inputLayer(7);
     NetworkLayer hiddenLayer(1);
     NetworkLayer outputLayer(2);
 
     ann.add(inputLayer, "input").add(hiddenLayer, "hidden").add(outputLayer, "output");
     ann.showLayers();
+
+    //initializing the array of data
+    float data[210][8] = {{0.0}};
+    Util util ;
+    util.loadCSV("./data/seeds_dataset.csv", data);
+   
+    util.printData(data);
+    
+    return 0 ;
 }
